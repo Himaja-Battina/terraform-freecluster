e833508d3ac0780fe912b7b6cf8ee006ab789cf2
Himaja-Battina <124758137+Himaja-Battina@users.noreply.github.com>
2023-03-27 12:51:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
 variable "cluster_name" {
   description = "Name of the cluster"
   type        = string
+  default="test-free"
 }
-
+variable "region"{
+  description="region"
+  type=string
+  default="us-east"
+  
+}

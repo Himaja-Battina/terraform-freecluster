62f1012968a546b5741fb3266a473ba8f79a5e94
Himaja-Battina <124758137+Himaja-Battina@users.noreply.github.com>
2023-03-27 12:47:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,6 @@
 resource "ibm_container_cluster" "cluster" {
   name         = var.cluster_name
-  datacenter   = var.worker_zone
-  hardware     = var.hardware
+  region=var.region
   machine_type = "free"
-
-  dynamic taints {
-    for_each = (var.taints != null ? var.taints : [])
-    content {
-      key    = taints.value.key
-      value  = taints.value.value
-      effect = taints.value.effect
-    }
-  }
-
-  timeouts {
-    create = (var.create_timeout != null ? var.create_timeout : null)
-    update = (var.update_timeout != null ? var.update_timeout : null)
-    delete = (var.delete_timeout != null ? var.delete_timeout : null)
-  }
 
 }

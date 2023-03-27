f300a3f272a002affb7dd2edd98a123f3a2844ea
Himaja-Battina <124758137+Himaja-Battina@users.noreply.github.com>
2023-03-27 13:02:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "ibm_container_cluster" "cluster" {
-  name         = var.cluster_name
+resource "ibm_container_cluster" "testacc_cluster" {
+  name            = var.name
+  datacenter      = "dal10"
+  machine_type    = "free"
+  hardware        = "shared"
   region=var.region
-  machine_type = "free"
-
 }
